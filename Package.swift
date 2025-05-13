aa5ecb10db2920c1e12df720d31a4917934fff53
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-13 12:44:44 +0800

    V0.0.2 创建模块，从 Whooshing.toolbox-basic 剥离

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "whooshing.toolbox-pgsql",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library( name: "PgSQL", targets: ["PgSQL"] ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SJJC-Team/whooshing-fluent.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.10.0"),
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", .upToNextMajor(from: "1.2.3"))
+    ],
+    targets: [
+        .target(
+            name:  "PgSQL",
+            dependencies: [
+                .product(name: "ErrorHandle", package: "whooshing.toolbox-basic"),
+                .product(name: "Fluent", package: "whooshing-fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+            ]
+        ),
+        .testTarget(
+            name: "toolbox-pgsql-Tests",
+            dependencies: [
+                .target(name: "PgSQL"),
+                .product(name: "Fluent", package: "whooshing-fluent")
+            ]
+        ),
+    ]
+)
