5164ad65ae96ae5fd7b1a30950b0e4112f70a2f5
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-16 13:12:59 +0800

    V1.0.1.1 更新错误处理

--- Sources/PgSQL/PgSQL.swift
+++ Sources/PgSQL/PgSQL.swift
@@ -309,10 +309,10 @@ public extension PGMigration {
 
         return s.create().flatMap {
             guard encrypt == true else { return database.eventLoop.makeSucceededVoidFuture() }
-            guard let db = database as? PostgresDatabase else { return database.eventLoop.future(error: PgErr.dataBaseError.d("数据库类型不是 PostgreSQL", 1023, (#file, #line))) }
+            guard let db = database as? PostgresDatabase else { return database.eventLoop.future(error: PgErr.dataBaseError.d("数据库类型不是 PostgreSQL", 1023)) }
             return db.query("ALTER TABLE \(name) SET ACCESS METHOD tde_heap;").flatMapError { err in
-                database.schema(name).delete().flatMapError { return database.eventLoop.future(error: PgErr.dataBaseTdeError.d("恢复失败", 1025, (#file, #line)).subErr($0))}
-                .flatMap { _ in database.eventLoop.future(error: PgErr.dataBaseTdeError.d("加密未成功，已删除该表格", 1024, (#file, #line)).subErr(err)) }
+                database.schema(name).delete().flatMapError { return database.eventLoop.future(error: PgErr.dataBaseTdeError.d("恢复失败", 1025).subErr($0))}
+                .flatMap { _ in database.eventLoop.future(error: PgErr.dataBaseTdeError.d("加密未成功，已删除该表格", 1024).subErr(err)) }
             }.transform(to: ())
         }
     }
@@ -384,7 +384,7 @@ fileprivate extension PGFields {
         var properties: [PGFieldParam] = []
         let mirror = Mirror(reflecting: self)
         for case let (_, value) in mirror.children {
-            guard let val = value as? PGFieldParam else { fatalError(PgErr.fieldDefineError.d("解析失败", 1020, (#file, #line)).description) }
+            guard let val = value as? PGFieldParam else { fatalError(PgErr.fieldDefineError.d("解析失败", 1020).description) }
             properties.append(val)
         }
         return properties
