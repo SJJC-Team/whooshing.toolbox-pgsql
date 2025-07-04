be08b67414e9c37735bb32f28ae1c59030c9334a
ChenLin Wang <wangchenlin2001@icloud.com>
2025-07-04 14:22:31 +0800

    V1.0.3.3 修复依赖

--- Tests/toolbox-pgsql-Tests/PgSQL-Tests.swift
+++ Tests/toolbox-pgsql-Tests/PgSQL-Tests.swift
@@ -17,9 +17,9 @@ struct PGSQLTests {
                     configuration: .init(
                         hostname: "localhost",
                         port: 5432,
-                        username: "testing",
+                        username: "clwang",
                         password: "testing",
-                        database: "testing",
+                        database: "postgres",
                         tls: .disable
                     )
                 ),
