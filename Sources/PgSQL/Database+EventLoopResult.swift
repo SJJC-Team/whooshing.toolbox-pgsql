ee539aac9e87a8349da3add6aa6df2718129d972
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-23 23:44:43 +0800

    V1.1.1.1 修复数据库 transaction 时可能出现未捕获错误的 bug

--- Sources/PgSQL/Database+EventLoopResult.swift
+++ Sources/PgSQL/Database+EventLoopResult.swift
@@ -4,9 +4,39 @@ import NIOAdvanced
 public extension Database {
     /// 使用自定义错误类型封装的事务执行器。
     @inlinable
-    func trans<T, G>(_ closure: @escaping @Sendable (Self) -> EventLoopResult<T, G>) -> EventLoopResult<T, G> {
+    func trans<T, G: Err>(
+        throws error: G.ErrorList,
+        _ explain: String? = nil,
+        metadata: Logger.Metadata? = nil,
+        category: ErrCategory,
+        file: String = #fileID,
+        line: Int = #line,
+        function: String = #function,
+        _ closure: @escaping @Sendable (Self) -> EventLoopResult<T, G>,
+    ) -> EventLoopResult<T, G> {
         self.trans { db in
             closure(db).wrapped
+        }.flatMapError { e in
+            if let err = e as? G {
+                return eventLoop.makeFailedFuture(err)
+            }
+            return eventLoop.makeFailedFuture(G.init(error, explain, category: category, file: file, line: line, function: function).subErr(e).metadata(metadata))
+        }.withError()
+    }
+    
+    /// 使用自定义错误类型封装的事务执行器。
+    @inlinable
+    func trans<T, G>(
+        throws error: G,
+        _ closure: @escaping @Sendable (Self) -> EventLoopResult<T, G>,
+    ) -> EventLoopResult<T, G> {
+        self.trans { db in
+            closure(db).wrapped
+        }.flatMapError { e in
+            if let err = e as? G {
+                return eventLoop.makeFailedFuture(err)
+            }
+            return eventLoop.makeFailedFuture(error)
         }.withError()
     }
     
