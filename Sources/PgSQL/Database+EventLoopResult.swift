53a97075e0369a981f767f80642b7394191b2765
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-23 23:58:54 +0800

    V1.1.1.3 增加 atrans 扩展函数

--- Sources/PgSQL/Database+EventLoopResult.swift
+++ Sources/PgSQL/Database+EventLoopResult.swift
@@ -50,9 +50,49 @@ public extension Database {
     
     /// 在 async/await 环境中执行数据库事务。
     @inlinable
-    func trans<T: Sendable>(_ closure: @escaping @Sendable (Self) async throws -> T) async throws -> T {
-        try await self.transaction { db in
-            try await closure(db as! Self)
+    func atrans<T: Sendable, G: Err>(
+        throws error: G.ErrorList,
+        _ explain: String? = nil,
+        metadata: Logger.Metadata? = nil,
+        category: ErrCategory,
+        file: String = #fileID,
+        line: Int = #line,
+        function: String = #function,
+        _ closure: @escaping @Sendable (Self) async throws(G) -> T
+    ) async throws(G) -> T {
+        do {
+            return try await self.transaction { db throws(G) in
+                do {
+                    return try await closure(db as! Self)
+                } catch {
+                    throw error as! G
+                }
+            }
+        } catch let e as G {
+            throw e
+        } catch let e {
+            throw G.init(error, explain, category: category, file: file, line: line, function: function).metadata(metadata).subErr(e)
+        }
+    }
+    
+    /// 在 async/await 环境中执行数据库事务。
+    @inlinable
+    func atrans<T: Sendable, E>(
+        throws error: E,
+        _ closure: @escaping @Sendable (Self) async throws(E) -> T
+    ) async throws(E) -> T {
+        do {
+            return try await self.transaction { db throws(E) in
+                do {
+                    return try await closure(db as! Self)
+                } catch {
+                    throw error as! E
+                }
+            }
+        } catch let e as E {
+            throw e
+        } catch let e {
+            throw error
         }
     }
 }
