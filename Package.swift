603f7f21a503447978704ffc6dd565b776596454
ChenLin Wang <wangchenlin2001@icloud.com>
2026-08-18 15:21:25 +0800

    V1.1.2.1 更新依赖

--- Package.swift
+++ Package.swift
@@ -15,28 +15,28 @@ let package = Package(
         .library( name: "PgSQL", targets: ["PgSQL"] ),
     ],
     dependencies: [
-        .package(url: "https://github.com/whooshing-workshop/whooshing-fluent.git", from: "1.0.3"),
-        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-basic.git", from: "1.6.2"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.13.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.10.0"),
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.0")
+        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.0"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-basic.git", from: "1.6.2")
     ],
     targets: [
         .target(
             name:  "PgSQL",
             dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "AnyCodable", package: "AnyCodable"),
                 .product(name: "ErrorHandle", package: "whooshing.toolbox-basic"),
                 .product(name: "NIOAdvanced", package: "whooshing.toolbox-basic"),
-                .product(name: "LoggingAdvanced", package: "whooshing.toolbox-basic"),
-                .product(name: "Fluent", package: "whooshing-fluent"),
-                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
-                .product(name: "AnyCodable", package: "AnyCodable")
+                .product(name: "LoggingAdvanced", package: "whooshing.toolbox-basic")
             ]
         ),
         .testTarget(
             name: "toolbox-pgsql-Tests",
             dependencies: [
                 .target(name: "PgSQL"),
-                .product(name: "Fluent", package: "whooshing-fluent")
+                .product(name: "Fluent", package: "fluent")
             ]
         ),
     ]
